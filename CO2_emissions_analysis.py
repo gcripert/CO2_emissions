2c27be53e779db6e0768d53bd65ca082a9eacd1d
gcripert <122497730+gcripert@users.noreply.github.com>
2023-03-21 16:07:33 -0400

    Adding updated code

--- CO2_emissions_analysis.py
+++ CO2_emissions_analysis.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import pycountry_convert as pc
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
 
 def country_to_continent(country_name):
     #print(country_name)
@@ -61,7 +66,89 @@ df = add_continent_column(df)
 
 
 
+#below: summing emissions by type/continent and preparing to graph
+
+df_europe = df[df['Continent'] == 'Europe']
+df_europe = df_europe.groupby('Year').sum(numeric_only = True)
+
+df_na = df[df['Continent'] == 'North America']
+df_na = df_na.groupby('Year').sum(numeric_only = True)
+
+df_asia = df[df['Continent'] == 'Asia']
+df_asia = df_asia.groupby('Year').sum(numeric_only = True)
+
+df_africa = df[df['Continent'] == 'Africa']
+df_africa = df_africa.groupby('Year').sum(numeric_only = True)
+
+df_sa = df[df['Continent'] == 'South America']
+df_sa = df_sa.groupby('Year').sum(numeric_only = True)
+
+df_oceania = df[df['Continent'] == 'Oceania']
+df_oceania = df_oceania.groupby('Year').sum(numeric_only = True)
 
 
-print (df)
+
+
+
+
+
+ax = plt.plot(df_europe['Total'], label = 'Europe')
+
+ax = plt.plot(df_na['Total'], label = 'North America')
+
+ax = plt.plot(df_asia['Total'], label = 'Asia')
+
+ax = plt.plot(df_africa['Total'], label = 'Africa')
+
+ax = plt.plot(df_sa['Total'], label = 'South America')
+
+ax = plt.plot(df_oceania['Total'], label = 'Oceania')
+
+
+plt.legend(title = 'Country')
+plt.ylabel('Megatons CO2')
+plt.xlabel('Year')
+plt.title('CO2 Emissions (Total)')
+plt.show()
+
+
+ax = plt.plot(df_europe['Per Capita'], label = 'Europe')
+
+ax = plt.plot(df_na['Per Capita'], label = 'North America')
+
+ax = plt.plot(df_asia['Per Capita'], label = 'Asia')
+
+ax = plt.plot(df_africa['Per Capita'], label = 'Africa')
+
+ax = plt.plot(df_sa['Per Capita'], label = 'South America')
+
+ax = plt.plot(df_oceania['Per Capita'], label = 'Oceania')
+
+
+
+
+              
+
+
+plt.legend(title = 'Country')
+plt.ylabel('Megatons CO2')
+plt.xlabel('Year')
+plt.title('CO2 Emissions (Per Capita)')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+#df.to_csv('test.csv')
+
+
+
+#print (df_europe['Year'])
 
